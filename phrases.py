819dba805f476dc3c41a32bf775e5a0561ad299a
Nicole Gentile <nicole.gentile98@gmail.com>
2025-11-17 21:37:52 +0100

    Add sample phrases to guess

---
+++ phrases.py
@@ -0,0 +1,6 @@
+from base_models import Phrase
+
+phrases = [
+    Phrase(title='Colombo', phrase_to_guess="Scopre l'America"),
+    Phrase(title='Italia', phrase_to_guess='Ha la bandiera tricolore'),
+]
